9a12861903d7f26cecbcd04b342e339e6a0b9ffc
Daniel Evans <daniel.e2718@gmail.com>
2018-06-11 10:57:42 -0500

    added docstring, defined `compose`
    `compose` is used in `preprocess.py` as higher-level logic for
    applying several preprocessing operations to items

--- common.py
+++ common.py
@@ -1,3 +1,13 @@
+"""Provide common definitions"""
+
 from collections import namedtuple
+from functools import reduce, partial
 
 Item = namedtuple('Item', 'title text url')
+
+def compose(*funcs):
+    """Return the composition of funcs as a function of one variable.
+
+The innermost function is the first given, ie:
+compose(f, g, h)(x) -> h(g(f(x)))"""
+    return partial(reduce, lambda f, g: g(f), funcs)
